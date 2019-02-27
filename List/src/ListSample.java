3a34ffb75fe522a401cc6c83e859e3ce2f4f04f4
maifujii <b1100136@gl.aiu.ac.jp>
2019-02-27 19:50:47 +0900

    0227

---
+++ List/src/ListSample.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSample{
+	
+	public static void main(String[]arge){
+		List<String>list = new ArrayList<String>();
+		
+		//値の記憶はadd メソッドを利用
+	list.add("1");
+	list.add("2");
+	list.add("3");
+	list.add("4");
+	list.add("5");
+	
+	//値の取得
+	for(int i=0;i<list.size();i++){
+		//値の取得はgetメソッドを利用
+		System.out.println(list.get(i));
+		
+	}
+	
+for(String s :list){
+	System.out.println(s);
+	}
+}}
